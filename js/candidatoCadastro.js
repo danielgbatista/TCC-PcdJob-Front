e7e5c74468a3256add48b1f3d74633e33c09925a
Daniel Gonçalves Batista <88516108+danielgbatista@users.noreply.github.com>
2022-04-13 16:50:32 -0300

    Resolvendo erros do consumo de api

--- js/candidatoCadastro.js
+++ js/candidatoCadastro.js
@@ -1,16 +1,32 @@
 "use strict";
 
-const postCandidato = async (candidato) => {
+const postCandidato =  async(candidato) => {
     const urlCadastro = 'http://10.107.144.20:8080/candidato/cadastrar'
     const options = {
         method: 'POST',
-        body: JSON.stringify(candidato),
+        body:candidato,
         headers: {
-            // 'content-Type' : 'application/json'
+            'Content-Type': 'application/json',
+            'Accept':'application/json',
+            'Access-Control-Allow-Headers': '*'
         },
-        mode: 'no-cors'
+        mode: 'cors'
     }
-    await fetch(urlCadastro, options)
+
+    await fetch(urlCadastro, options).then(resp=>console.log(resp))
+   
+
 }
 
-export {postCandidato}
+const getCandidato = () => {
+
+    const urlListar = 'http://10.107.144.20:8080/candidato/listar'
+    const options = {
+        method: 'GET',
+        mode: 'cors'
+    }
+    fetch(urlListar, options).then(resp=>console.log(resp))
+
+}
+
+export {postCandidato, getCandidato}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import { postCandidato } from "./candidatoCadastro.js" ;
+import { postCandidato, getCandidato} from "./candidatoCadastro.js" ;
 
-const c = document.querySelector('#txtNome').value;
+// const c = document.querySelector('#txtNome').value;
 
-console.log(c)
 
-// const cadastrarCandidato =  async() => {
+
+// const cadastrarCandidato =  async () => {
 //     const candidato = {
 //         nome: document.getElementById('txtNome').value,
 //         email: document.getElementById('txtEmail').value,
@@ -14,7 +14,7 @@ console.log(c)
 //         genero: "PREFIRO_NAO_INFORMAR"
 //     }
 
-//    await postCandidato(candidato)
+//     await postCandidato(candidato)
 
 // }
 
@@ -26,12 +26,15 @@ const nome = document.getElementById('txtNome').value
 const email = document.getElementById('txtEmail').value
 const senha = document.getElementById('pwdSenha').value
 
-console.log(nome    )
+
+
 let url = 'http://10.107.144.20:8080/candidato/cadastrar';
 
-        console.log(fetch(url, {method: 'POST', headers: {
-            'Content-Type': 'application/json',     
-        },mode:"no-cors", body:JSON.stringify( {email: email,  nome: nome, senha:senha, genero: 'PREFIRO_NAO_INFORMAR'}),
+        console.log(fetch(url, {method: 'POST', headers: new Headers({
+            'Content-Type': 'application/json',
+            'Accept':'application/json',
+            'Access-Control-Allow-Headers': '*'   
+        }), body:{email: 'dsadsa@dsda',  nome: 'danizinho', senha:'1234', genero: 'PREFIRO_NAO_INFORMAR'},
         }))
 
 }
