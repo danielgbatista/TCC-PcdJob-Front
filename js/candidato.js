a2aa470d7537d9f5fc5b7946651516ec748f559f
danielgbatista <daniel.g.batista21@outlook.com>
2022-04-18 20:59:17 -0300

    Finalizado métodos de API para candidato e empresa

--- js/candidato.js
+++ js/candidato.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const postCandidato =  async(candidato) => {
-    const urlCadastro = 'http://10.107.144.24:8080/candidato/cadastrar'
+    const urlCadastrar = 'http://10.107.144.24:8080/candidato/cadastrar'
     const options = {
         method: 'POST',
         body:JSON.stringify(candidato),
@@ -12,26 +12,36 @@ const postCandidato =  async(candidato) => {
         mode: 'cors'
     }
 
-    await fetch(urlCadastro, options).then(resp=>console.log(resp))
+    await fetch(urlCadastrar, options).then(resp=>console.log(resp))
    
 
 }
 
-const putCandidato =  async(candidato) => {
-    const urlCadastro = `http://10.107.144.24:8080/candidato/atualizar`
+const putProduto = async (candidato) => {
+    const urlAtualizar = 'http://10.107.144.24:8080/candidato/atualizar/'
     const options = {
         method: 'PUT',
-        body:JSON.stringify(candidato),
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Accept':'application/json'
-        }),
-        mode: 'cors'
+        body: JSON.stringify(candidato),
+        headers: {
+            'content-Type' : 'application/json'
+        },
+        mode:'cors'
     }
 
-    await fetch(urlCadastro, options).then(resp=>console.log(resp))
-   
+    await fetch(`${urlAtualizar}${candidato.id}`, options)
+}
 
+const deleteProduto = async (candidato) => {
+    const urldeletar = 'http://10.107.144.24:8080/candidato/deletar/'
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'content-Type' : 'application/json'
+        },
+        mode:'cors'
+    }
+
+    await fetch(`${urldeletar}${candidato.id}`, options)
 }
 
 const getCandidato = () => {
--- js/empresa.js
+++ js/empresa.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const postEmpresa =  async(candidato) => {
+const postEmpresa =  async(empresa) => {
     const urlCadastro = 'http://10.107.144.24:8080/empresa/cadastrar'
     const options = {
         method: 'POST',
-        body:candidato,
+        body:empresa,
         headers: {
             'Content-Type': 'application/json',
             'Accept':'application/json',
@@ -17,21 +17,31 @@ const postEmpresa =  async(candidato) => {
 
 }
 
-const putEmpresa =  async(candidato) => {
-    const urlCadastro = 'http://10.107.144.24:8080/empresa/atualizar'
+const putProduto = async (empresa) => {
+    const urlAtualizar = 'http://10.107.144.24:8080/empresa/atualizar/'
     const options = {
         method: 'PUT',
-        body:candidato,
+        body: JSON.stringify(empresa),
         headers: {
-            'Content-Type': 'application/json',
-            'Accept':'application/json',
+            'content-Type' : 'application/json'
         },
-        mode: 'cors'
+        mode:'cors'
     }
 
-    await fetch(urlCadastro, options).then(resp=>console.log(resp))
-   
+    await fetch(`${urlAtualizar}${empresa.id}`, options)
+}
 
+const deleteProduto = async (empresa) => {
+    const urldeletar = 'http://10.107.144.24:8080/empresa/deletar/'
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'content-Type' : 'application/json'
+        },
+        mode:'cors'
+    }
+
+    await fetch(`${urldeletar}${empresa.id}`, options)
 }
 
 const getEmpresa = () => {
